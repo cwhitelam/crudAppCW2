ca57c4d4cba332ebacf2a0c05e5f7a52d953ef1a
cwhitelam <cwhitelam70@gmail.com>
2023-02-20 11:23:22 -0500

    dev commit

---
+++ crudApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crudApp.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Please enter your name")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Please enter your email")]
+    public string Email { get; set; }
+
+    public string RoleId { get; set; }
+}
---
+++ crudApp/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿// using crudApp.Data;
+// using crudApp.Models;
+// using Microsoft.EntityFrameworkCore;
+//
+//
+// namespace crudApp.Services
+// {
+//     public class UserService  
+//     {
+//         private readonly AppDbContext _context;
+//
+//         public UserService(AppDbContext context)
+//         {
+//             _context = context;
+//         }
+//
+//         public async Task<List<User>?> GetUsersAsync()
+//         {
+//             return await _context.Users.ToListAsync();
+//         }
+//
+//         public async Task<User?> GetUserAsync(int id)
+//         {
+//             return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+//         }
+//
+//         public async Task AddUserAsync(User? user)
+//         {
+//             _context.Users.Add(user);
+//             await _context.SaveChangesAsync();
+//         }
+//
+//         public async Task UpdateUserAsync(User user)
+//         {
+//             _context.Entry(user).State = EntityState.Modified;
+//             await _context.SaveChangesAsync();
+//         }
+//
+//         public async Task DeleteUserAsync(int id)
+//         {
+//             var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+//             _context.Users.Remove(user);
+//             await _context.SaveChangesAsync();
+//         }
+//     }
+// }
