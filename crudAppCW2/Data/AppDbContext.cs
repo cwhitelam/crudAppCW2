5bbfe24de5a6cb5d8747ce9153c958e11598f609
cwhitelam <cwhitelam70@gmail.com>
2023-02-22 22:11:44 -0500

    add OnModelCreating

--- crudAppCW2/Data/AppDbContext.cs
+++ crudAppCW2/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
+﻿using crudAppCW2.Data.Models;
 using crudAppCW2.Models;
+using crudAppCW2.Pages;
 using Microsoft.EntityFrameworkCore;
 
 namespace crudAppCW2.Data;
@@ -20,5 +21,30 @@ public class AppDbContext : DbContext
         optionsBuilder.UseSqlite("Data Source=Data/crudAppDb.sqlite");
     }
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Department> Departments => Set<Department>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Department>().HasData(
+            new Department { Id = 1, Name = "Admin" },
+            new Department { Id = 2, Name = "Engineering" },
+            new Department { Id = 3, Name = "Golf Course" },
+            new Department { Id = 4, Name = "Human Resources" },
+            new Department { Id = 5, Name = "IT" });
+
+        /*modelBuilder.Entity<Employee>().HasData(
+            new Employee { Id = 1, FirstName = "Anna", LastName = "Rockstar", DepartmentId = 2 },
+            new Employee { Id = 2, FirstName = "Julia", LastName = "Developer", DepartmentId = 5, IsDeveloper = true },
+            new Employee { Id = 3, FirstName = "Thomas", LastName = "Huber", DepartmentId = 5, IsDeveloper = true },
+            new Employee { Id = 4, FirstName = "Sara", LastName = "Metroid", DepartmentId = 1 },
+            new Employee { Id = 5, FirstName = "Ben", LastName = "Rockstar", DepartmentId = 4 },
+            new Employee { Id = 6, FirstName = "Alex", LastName = "Rider", DepartmentId = 3, IsDeveloper = true },
+            new Employee { Id = 7, FirstName = "Sophie", LastName = "Ramos", DepartmentId = 5 },
+            new Employee { Id = 8, FirstName = "Julien", LastName = "Russell", DepartmentId = 2 },
+            new Employee { Id = 9, FirstName = "Yvonne", LastName = "Snider", DepartmentId = 4 },
+            new Employee { Id = 10, FirstName = "Jasmin", LastName = "Curtis", DepartmentId = 1, IsDeveloper = true });*/
+    }
 }
