ea8398a56ab9121ce7c7251742892236f436b3eb
cwhitelam <cwhitelam70@gmail.com>
2023-02-27 17:21:41 -0500

    update DbContext with DbSets

--- crudAppCW2/Migrations/AppDbContextModelSnapshot.cs
+++ crudAppCW2/Migrations/AppDbContextModelSnapshot.cs
@@ -30,7 +30,7 @@ namespace crudAppCW2.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Departments");
+                    b.ToTable("Department");
 
                     b.HasData(
                         new
@@ -58,6 +58,36 @@ namespace crudAppCW2.Migrations
                             Id = 5,
                             Name = "IT"
                         });
+                });
+
+            modelBuilder.Entity("crudAppCW2.Data.Models.Role", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Role");
+                });
+
+            modelBuilder.Entity("crudAppCW2.Data.Models.UserRole", b =>
+                {
+                    b.Property<int>("UserId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("RoleId")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("UserId", "RoleId");
+
+                    b.HasIndex("RoleId");
+
+                    b.ToTable("UserRole");
                 });
 
             modelBuilder.Entity("crudAppCW2.Models.User", b =>
@@ -88,19 +118,58 @@ namespace crudAppCW2.Migrations
 
                     b.HasIndex("DepartmentId");
 
-                    b.ToTable("Users");
+                    b.HasIndex("RoleId");
+
+                    b.ToTable("User");
+                });
+
+            modelBuilder.Entity("crudAppCW2.Data.Models.UserRole", b =>
+                {
+                    b.HasOne("crudAppCW2.Data.Models.Role", "Role")
+                        .WithMany("UserRoles")
+                        .HasForeignKey("RoleId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("crudAppCW2.Models.User", "User")
+                        .WithMany("UserRoles")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Role");
+
+                    b.Navigation("User");
                 });
 
             modelBuilder.Entity("crudAppCW2.Models.User", b =>
                 {
-                    b.HasOne("crudAppCW2.Data.Models.Department", null)
+                    b.HasOne("crudAppCW2.Data.Models.Department", "Department")
                         .WithMany("Users")
                         .HasForeignKey("DepartmentId");
+
+                    b.HasOne("crudAppCW2.Data.Models.Role", "Role")
+                        .WithMany()
+                        .HasForeignKey("RoleId");
+
+                    b.Navigation("Department");
+
+                    b.Navigation("Role");
                 });
 
             modelBuilder.Entity("crudAppCW2.Data.Models.Department", b =>
                 {
                     b.Navigation("Users");
+                });
+
+            modelBuilder.Entity("crudAppCW2.Data.Models.Role", b =>
+                {
+                    b.Navigation("UserRoles");
+                });
+
+            modelBuilder.Entity("crudAppCW2.Models.User", b =>
+                {
+                    b.Navigation("UserRoles");
                 });
 #pragma warning restore 612, 618
         }
