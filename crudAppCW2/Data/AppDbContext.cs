4df911df7ae6dd8e15841f0fdd0d026cac17f5f4
cwhitelam <cwhitelam70@gmail.com>
2023-02-27 17:21:13 -0500

    update DbContext with DbSets

--- crudAppCW2/Data/AppDbContext.cs
+++ crudAppCW2/Data/AppDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using crudAppCW2.Data.Models;
 using crudAppCW2.Models;
-using crudAppCW2.Pages;
 using Microsoft.EntityFrameworkCore;
 
 namespace crudAppCW2.Data;
@@ -21,30 +20,38 @@ public class AppDbContext : DbContext
         optionsBuilder.UseSqlite("Data Source=Data/crudAppDb.sqlite");
     }
 
-    public DbSet<User> Users => Set<User>();
-    public DbSet<Department> Departments => Set<Department>();
-    
+    public DbSet<User> User => Set<User>();
+    public DbSet<Department> Department => Set<Department>();
+    public DbSet<UserRole> UserRole => Set<UserRole>();
+    public DbSet<Role> Role => Set<Role>();
+
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Role>().HasData(
+            new Role {Id = 1, Name = "Admin" },
+            new Role {Id = 2, Name = "User" });
 
         modelBuilder.Entity<Department>().HasData(
-            new Department { Id = 1, Name = "Admin" },
-            new Department { Id = 2, Name = "Engineering" },
-            new Department { Id = 3, Name = "Golf Course" },
-            new Department { Id = 4, Name = "Human Resources" },
-            new Department { Id = 5, Name = "IT" });
+            new Department { Id = 1, Name = "Commercial" },
+            new Department { Id = 2, Name = "Golf Course" },
+            new Department { Id = 3, Name = "Maintenance" },
+            new Department { Id = 4, Name = "IT" },
+            new Department { Id = 5, Name = "Admin" });
 
-        /*modelBuilder.Entity<Employee>().HasData(
-            new Employee { Id = 1, FirstName = "Anna", LastName = "Rockstar", DepartmentId = 2 },
-            new Employee { Id = 2, FirstName = "Julia", LastName = "Developer", DepartmentId = 5, IsDeveloper = true },
-            new Employee { Id = 3, FirstName = "Thomas", LastName = "Huber", DepartmentId = 5, IsDeveloper = true },
-            new Employee { Id = 4, FirstName = "Sara", LastName = "Metroid", DepartmentId = 1 },
-            new Employee { Id = 5, FirstName = "Ben", LastName = "Rockstar", DepartmentId = 4 },
-            new Employee { Id = 6, FirstName = "Alex", LastName = "Rider", DepartmentId = 3, IsDeveloper = true },
-            new Employee { Id = 7, FirstName = "Sophie", LastName = "Ramos", DepartmentId = 5 },
-            new Employee { Id = 8, FirstName = "Julien", LastName = "Russell", DepartmentId = 2 },
-            new Employee { Id = 9, FirstName = "Yvonne", LastName = "Snider", DepartmentId = 4 },
-            new Employee { Id = 10, FirstName = "Jasmin", LastName = "Curtis", DepartmentId = 1, IsDeveloper = true });*/
+        modelBuilder.Entity<UserRole>()
+            .HasKey(ud => new { ud.UserId, DepartmentId = ud.RoleId });
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ud => ud.User)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ud => ud.UserId);
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ud => ud.Role)
+            .WithMany(d => d.UserRoles)
+            .HasForeignKey(ud => ud.RoleId);
     }
 }
