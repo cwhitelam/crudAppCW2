0309cd61be02d60eea8675d0ef89d76022f7065c
cwhitelam <cwhitelam70@gmail.com>
2023-03-01 19:17:26 -0500

    no commit

--- crudAppCW2/Data/Models/User.cs
+++ crudAppCW2/Data/Models/User.cs
@@ -3,26 +3,18 @@ using crudAppCW2.Data.Models;
 
 namespace crudAppCW2.Models;
 
-
 public class User
-    {
-        public int Id { get; set; }
-    
-        [Required(ErrorMessage = "Please enter your first name")]
-        public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Please enter your last name")]
-        public string LastName { get; set; }
-        
-        [Required(ErrorMessage = "Please enter your email")]
-        public string Email { get; set; }
-
-        public int? DepartmentId { get; set; }
-        public int? RoleId { get; set; }
-        public Department? Department { get; set; }
-        public Role? Role { get; set; }
-        
-        public ICollection<UserRole> UserRoles { get; set; }
-
-    }
-    
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Please enter your first name")]
+    public string FirstName { get; set; }
+    [Required(ErrorMessage = "Please enter your last name")]
+    public string LastName { get; set; }
+    [Required(ErrorMessage = "Please enter your email")]
+    public string Email { get; set; }
+    public int? DepartmentId { get; set; }
+    public int? RoleId { get; set; }
+    public Department? Department { get; set; }
+    public Role? Role { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; }
+}
