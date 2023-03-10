8bbf70c14f488735c12a92f54bae62b2815dd1a2
cwhitelam <cwhitelam70@gmail.com>
2023-03-10 13:08:10 -0500

    commit migrations

---
+++ crudAppCW2/Migrations/20230310162754_DeleteRefactor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crudAppCW2.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteRefactor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "User");
+        }
+    }
+}
