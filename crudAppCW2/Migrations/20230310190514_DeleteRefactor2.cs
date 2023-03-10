05e5ccd69f52bc45e7e67efdf0b5d5713ddd90fb
cwhitelam <cwhitelam70@gmail.com>
2023-03-10 14:19:41 -0500

    commit migrations

---
+++ crudAppCW2/Migrations/20230310190514_DeleteRefactor2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crudAppCW2.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteRefactor2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "User",
+                newName: "Selected");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRestored",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRestored",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "Selected",
+                table: "User",
+                newName: "IsDeleted");
+        }
+    }
+}
---
+++ crudAppCW2/Migrations/20230310191054_RefactorUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crudAppCW2.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Selected",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Selected",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
