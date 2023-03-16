06f9d9c888658b78339c08ece654ad2764c67676
cwhitelam <cwhitelam70@gmail.com>
2023-03-16 19:49:45 -0400

    update Notification.cs class

--- crudAppCW2/Data/Models/Notification.cs
+++ crudAppCW2/Data/Models/Notification.cs
@@ -1,15 +1,18 @@
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace crudAppCW2.Data.Models
 {
     public class Notification
     {
         public int NotificationId { get; set; }
-        public string? Subject { get; set; }
         public string? Body { get; set; }
-        public int? RoleId { get; set; }
+        public string? Subject { get; set; }
+        public bool IsRead { get; set; }
+        [NotMapped]
+        public bool IsSelected { get; set; }
+        
+        public DateTime CreatedDateTime { get; set; }
+        public List<NotificationRole>? NotificationRoles { get; set; }
+        public List<NotificationUser>? NotificationUsers { get; set; }
     }
-
 }
-
-
