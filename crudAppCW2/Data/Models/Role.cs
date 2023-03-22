3c5602a212a73421576a7ed70342c86c22835072
cwhitelam <cwhitelam70@gmail.com>
2023-03-22 08:19:13 -0400

    removed models directive

--- crudAppCW2/Data/Models/Role.cs
+++ crudAppCW2/Data/Models/Role.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using crudAppCW2.Models;
 
 namespace crudAppCW2.Data.Models;
 
@@ -10,7 +9,7 @@ public class Role
     [Required(ErrorMessage = "Please enter a name for the role")]
     public string? Name { get; set; }
 
-    public List<User> Users { get; set; }
-    public IEnumerable<UserRole> UserRoles { get; set; }
-    public List<NotificationRole> NotificationRoles { get; set; }
+    public List<User>? Users { get; set; }
+    public IEnumerable<UserRole> UserRoles { get; set; } = new List<UserRole>();
+    public List<NotificationRole>? NotificationRoles { get; set; }
 }
